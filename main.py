94f55a16dcf9a03be2316a94fe46180a19b24d62
JokerHB <aifjhb0815@gmail.com>
2023-06-20 23:57:44 +0800

    -add basic training method

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import tkinter as tk
+from DRL.AC import AC
 from Gobang.Gobang import Gobang
 
 if __name__ == '__main__':
     root = tk.Tk()
     game = Gobang(root)
-    root.mainloop()
+    # root.mainloop()
+
+    ac = AC(input_size=15 * 15, hidden_size=512, output_size=15 * 15)
+    ac.train_model(env=game)
